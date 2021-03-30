26a7fa7410f9dd3fcba96896596147b7055fbfbb
andrew <andrew@debian.andrew>
2021-03-30 13:59:24 +0100

    test

---
+++ js/dash.js
@@ -0,0 +1,14 @@
+function receive(data){
+    let allCardDiv = document.querySelector('#all-cards');
+    
+    data.forEach(function (dataSet) {
+        let card = document.createElement('div');
+        let cardBody = document.createElement('div');
+        let title = document.createElement('h3');
+        let cardText = document.createElement('p');
+        let cardHelpBtn = document.createElement('button');
+
+
+
+    });
+}
