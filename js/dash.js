6e6f7ba936e912bc493c702266e5f479460ae6a4
andrew <andrew@debian.andrew>
2021-03-30 14:20:19 +0100

    display

--- js/dash.js
+++ js/dash.js
@@ -1,14 +1,36 @@
-function receive(data){
+function receive(problems, actualUser){
     let allCardDiv = document.querySelector('#all-cards');
+    let headerDiv = document.querySelector('#header');
     
-    data.forEach(function (dataSet) {
+
+    problems.forEach(function (problem) {
+                    
         let card = document.createElement('div');
         let cardBody = document.createElement('div');
         let title = document.createElement('h3');
         let cardText = document.createElement('p');
         let cardHelpBtn = document.createElement('button');
 
+        title.innerHTML = problem.title;
+        title.innerHTML = problem.cardtext; 
 
+        if(actualUser.type == 'therapist'){
+            allCardDiv.appendChild(card);
+            allCardDiv.appendChild(cardBody);
+            allCardDiv.appendChild(title);
+            allCardDiv.appendChild(cardText);
+            allCardDiv.appendChild(cardHelpBtn);
+        }else{
+            let newProblem = document.createElement('button');
+            newProblem.appendChild(headerDiv);
+
+            if(problem.userId == actualUser.id){
+                allCardDiv.appendChild(card);
+                allCardDiv.appendChild(cardBody);
+                allCardDiv.appendChild(title);
+                allCardDiv.appendChild(cardText);
+            }
+        }
 
     });
 }
